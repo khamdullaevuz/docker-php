af604692d0b685d579e21dc1f787cffe83f341c7
Elbek Khamdullaev <elbek.khamdullaev@gmail.com>
2022-12-15 14:32:35 +0500

    bug fix

--- app/config/Core.php
+++ app/config/Core.php
@@ -7,9 +7,9 @@ class Core
 {
     const APP_NAME = "MVC";
 
-    const HOSTNAME = "localhost";
+    const HOSTNAME = "192.168.31.119";
     const DBNAME = "docker-php";
-    const USERNAME = "root";
+    const USERNAME = "user";
     const PASSWORD = "secret";
     const OPTIONS = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
