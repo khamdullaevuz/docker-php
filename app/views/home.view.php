8e6edefc77376810d18105d302c30cfddbb40158
Elbek Khamdullaev <elbek.khamdullaev@gmail.com>
2022-12-17 12:02:15 +0500

    new

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -3,7 +3,7 @@ $title = "Homepage";
 require 'layouts/header.layout.php';
 ?>
 <div class="container">
-<h1>Hello World!</h1>
+<h1>Hello World</h1>
 </div>
 <?php
 require 'layouts/footer.layout.php';
